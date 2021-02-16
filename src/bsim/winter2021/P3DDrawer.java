d2b62d8cdbca346ef58b37673409bd7f79e604a9
s-yfang <55638266+s-yfang@users.noreply.github.com>
2021-02-16 13:28:01 -0500

    Update P3DDrawer.java

--- src/bsim/winter2021/P3DDrawer.java
+++ src/bsim/winter2021/P3DDrawer.java
@@ -63,6 +63,85 @@ public class P3DDrawer extends BSimP3DDrawer {
 	}
 	
 	/**
+	 * Draws two chemical field structures based on its defined parameters, with custom transparency (alpha) parameters.
+	 * @param field_A	The chemical field structure to be rendered.
+	 * @param field_B	The chemical field structure to be rendered.
+	 * @param c			Desired colour of the chemical field.
+	 * @param alphaGrad	Alpha per unit concentration of the field.
+	 * @param alphaMax	Maximum alpha value (enables better viewing).
+	 */
+	public void drawMixedConc2D(BSimChemicalField field_A, BSimChemicalField field_B, Color c_A, Color c_B, double alphaGrad, double alphaMax) {
+		int[] boxes;						// Number of boxes
+		double[] boxSize;					// Size of each box
+		double alpha = 0.0f;
+		
+		// Choose the minimum number of boxes
+		if ( field_A.getBoxes().length < field_B.getBoxes().length ) {
+			boxes = field_A.getBoxes();
+			boxSize = field_A.getBox();
+		}
+		else {
+			boxes = field_B.getBoxes();
+			boxSize = field_B.getBox();
+		}
+		
+		for(int i = 0; i < boxes[0]; i++) {
+			for(int j = 0; j < boxes[1]; j++) {					
+				p3d.pushMatrix();					
+				p3d.translate((float)(boxSize[0]*i+boxSize[0]/2), (float)(boxSize[1]*j+boxSize[1]/2));
+				
+				alpha = alphaGrad*field_A.getConc(i, j, 0) + alphaGrad*field_B.getConc(i, j, 0);
+				if (alpha > alphaMax) 
+					alpha = alphaMax;
+				
+				Color c = getColor( field_A.getConc(i, j, 0), field_B.getConc(i, j, 0));
+				
+				p3d.fill(c.getRed(), c.getGreen(), c.getBlue(), (float)alpha);
+				p3d.box((float)boxSize[0], (float)boxSize[1], 0); 	// Only draw the x and y dimensions
+				p3d.popMatrix();
+			}
+		}
+	}
+	
+	public static Color getColor( double conc_A, double conc_B ) {
+		double conc = conc_A - conc_B;
+		float value = 0.0f;						// Between 0 and 1
+		
+		double conc_threshold = 1000;
+		
+		if ( conc >= conc_threshold ) {
+			value = 1.0f;
+		}
+		else if ( conc <= -conc_threshold ) {
+			value = -1.0f;
+		}
+		else {
+			value = (float) (conc/conc_threshold);
+		}
+		
+		float max_hue = 0f;//2f;								
+		//float min_hue = Color.RGBtoHSB(base_color.getRed(), base_color.getGreen(), base_color.getBlue(), null)[0];						// Red
+		float min_hue = 1f;					// Red
+		
+		//System.out.println(value);
+		
+		float hue = value * max_hue + ( 1 - value ) * min_hue;
+		
+		return new Color( Color.HSBtoRGB(hue, 1f, 1f) );
+	}
+	
+	/**
+	 * Draw a chemical field structure based on its defined parameters (default alpha).
+	 * @param field_A The chemical field to be drawn.
+	 * @param field_B The chemical field to be drawn.
+	 * @param c The desired colour.
+	 * @param alphaGrad The alpha-per-unit-concentration.
+	 */
+	public void drawMixedConc2D(BSimChemicalField field_A, BSimChemicalField field_B, Color c_A, Color c_B, float alphaGrad) {
+		drawMixedConc2D(field_A, field_B, c_A, c_B, alphaGrad, 255);
+	}
+	
+	/**
 	 * Draws two chemical field structures in a checkerboard pattern based on its defined parameters, with custom transparency (alpha) parameters.
 	 * @param field_A	The chemical field structure to be rendered.
 	 * @param field_B	The chemical field structure to be rendered.
