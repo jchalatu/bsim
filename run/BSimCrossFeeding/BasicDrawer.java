059834d062a7c7dd5025dbecbfda86e9ffcc8037
s-yfang <55638266+s-yfang@users.noreply.github.com>
2021-04-12 18:29:47 -0400

    Moved ticker and drawer into separate classes

---
+++ run/BSimCrossFeeding/BasicDrawer.java
@@ -0,0 +1,114 @@
+package BSimCrossFeeding;
+
+import bsim.BSim;
+import bsim.BSimChemicalField;
+import bsim.capsule.BSimCapsuleBacterium;
+import bsim.draw.BSimDrawer;
+import bsim.draw.BSimP3DDrawer;
+import processing.core.PConstants;
+import processing.core.PGraphics3D;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BasicDrawer extends BSimP3DDrawer{
+
+    // Bacterium should be a sub-class of BSimCapsuleBacterium
+    final ArrayList<CrossFeedingBacterium> bacA;
+    final ArrayList<CrossFeedingBacterium> bacB;
+    final double simX;
+    final double simY;
+    
+    public static BSimChemicalField amino_acid_A;
+    public static BSimChemicalField amino_acid_B;
+    public static double c; 
+
+    public BasicDrawer(BSim sim, int width_pixels, int height_pixels, double pixel_to_um_ratio, 
+    		ArrayList<CrossFeedingBacterium> bac_to_drawA, ArrayList<CrossFeedingBacterium> bac_to_drawB,
+    		BSimChemicalField amino_acid_A, BSimChemicalField amino_acid_B, double c) {
+        super(sim, width_pixels, height_pixels);
+        simX = width_pixels / pixel_to_um_ratio;
+        simY = height_pixels / pixel_to_um_ratio;
+        bacA = bac_to_drawA;
+        bacB = bac_to_drawB;
+        
+        BasicDrawer.amino_acid_A = amino_acid_A;
+        BasicDrawer.amino_acid_B = amino_acid_B;
+        BasicDrawer.c = c;
+    }
+
+    /**
+     * Draw the default cuboid boundary of the simulation as a partially transparent box
+     * with a wireframe outline surrounding it.
+     */
+    @Override
+    public void boundaries() {
+        p3d.noFill();
+        p3d.stroke(128, 128, 255);
+        p3d.pushMatrix();
+        p3d.translate((float)boundCentre.x,(float)boundCentre.y,(float)boundCentre.z);
+        p3d.box((float)bound.x, (float)bound.y, (float)bound.z);
+        p3d.popMatrix();
+        p3d.noStroke();
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        p3d.beginDraw();
+
+        if(!cameraIsInitialised){
+            // camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+            p3d.camera((float)bound.x*0.5f, (float)bound.y*0.5f,
+                    // Set the Z offset to the largest of X/Y dimensions for a reasonable zoom-out distance:
+                    simX > simY ? (float)simX : (float)simY,
+                    (float)bound.x*0.5f, (float)bound.y*0.5f, 0,
+                    0,1,0);
+            cameraIsInitialised = true;
+        }
+
+        p3d.textFont(font);
+        p3d.textMode(PConstants.SCREEN);
+
+        p3d.sphereDetail(10);
+        p3d.noStroke();
+        p3d.background(255, 255,255);
+
+        scene(p3d);
+        boundaries();
+        time();
+
+        p3d.endDraw();
+        g.drawImage(p3d.image, 0,0, null);
+    }
+
+    /**
+     * Draw the formatted simulation time to screen.
+     */
+    @Override
+    public void time() {
+        p3d.fill(0);
+        //p3d.text(sim.getFormattedTimeHours(), 50, 50);
+        p3d.text(sim.getFormattedTime(), 50, 50);
+    }
+
+    @Override
+    public void scene(PGraphics3D p3d) {
+        p3d.ambientLight(128, 128, 128);
+        p3d.directionalLight(128, 128, 128, 1, 1, -1);
+        
+		// Draw the amino acid field
+        draw2D(amino_acid_A, Color.BLUE, (float)(255/c));	
+        draw2D(amino_acid_B, Color.RED, (float)(255/c));	
+		
+		// Draw sub-population A
+		for(CrossFeedingBacterium b : bacA) {
+			draw(b, Color.GREEN );
+		}	
+		
+		// Draw sub-population B
+		for(CrossFeedingBacterium b : bacB) {
+			draw(b, Color.ORANGE );
+		}	
+
+    }
+}
---
+++ run/BSimCrossFeeding/BasicTicker.java
@@ -0,0 +1,206 @@
+package BSimCrossFeeding;
+
+import bsim.BSim;
+import bsim.BSimChemicalField;
+import bsim.BSimTicker;
+import bsim.capsule.BSimCapsuleBacterium;
+import bsim.capsule.Mover;
+import bsim.capsule.RelaxationMoverGrid;
+import com.beust.jcommander.Parameter;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BasicTicker extends BSimTicker {
+
+    BSim sim;
+    // logs data about time taken by ticker every LOG_INTERVAL timesteps
+    final int LOG_INTERVAL;
+    public boolean WITH_GROWTH = true;
+    static Random bacRng;
+    //growth rate standard deviation
+    public final double growth_stdv;
+    //growth rate mean
+    public final double growth_mean;
+    //elongation threshold standard deviation
+    public final double length_stdv;
+    //elongation threshold mean
+    public final double length_mean;
+
+    final ArrayList<CrossFeedingBacterium> bacA;
+    final ArrayList<CrossFeedingBacterium> bacB;
+    final ArrayList<BSimCapsuleBacterium> bacteriaAll;
+    final ArrayList<CrossFeedingBacterium> bac_bornA;
+    final ArrayList<CrossFeedingBacterium> bac_bornB;
+    final ArrayList<CrossFeedingBacterium> bac_deadA;
+    final ArrayList<CrossFeedingBacterium> bac_deadB;
+    
+    public static BSimChemicalField amino_acid_A;
+    public static BSimChemicalField amino_acid_B;
+
+    // internal machinery - don't worry about this
+    final Mover mover;
+
+    public BasicTicker(BSim sim, ArrayList<CrossFeedingBacterium> bacA, ArrayList<CrossFeedingBacterium> bacB,
+    		ArrayList<BSimCapsuleBacterium> bacteriaAll, int LOG_INTERVAL, Random bacRng, 
+    		double growth_stdv, double growth_mean, double length_stdv, double length_mean,
+    		BSimChemicalField amino_acid_A, BSimChemicalField amino_acid_B) {
+        this.sim = sim;
+        this.LOG_INTERVAL = LOG_INTERVAL;
+        this.bacRng = bacRng; //random number generator
+        this.growth_stdv = growth_stdv;
+        this.growth_mean = growth_mean;
+        this.length_stdv = length_stdv;
+        this.length_mean = length_mean;
+        this.bacA = bacA;
+        this.bacB = bacB;
+        this.bacteriaAll = bacteriaAll;
+        bac_bornA = new ArrayList();
+        bac_bornB = new ArrayList();
+        bac_deadA = new ArrayList();
+        bac_deadB = new ArrayList();
+        mover = new RelaxationMoverGrid(bacteriaAll, sim);
+        
+        BasicTicker.amino_acid_A = amino_acid_A;
+        BasicTicker.amino_acid_B = amino_acid_B;
+    }
+    
+    /** Function for bacteria growth activities. */
+    public void growBacteria( ArrayList<CrossFeedingBacterium> bac, ArrayList<CrossFeedingBacterium> bacBorn ) {
+    	Random bacRng = new Random(); 			// Random number generator
+    	bacRng.setSeed(50); 					// Initializes random number generator
+    	
+        for (CrossFeedingBacterium b : bac) { 				// Loop over bac array
+            b.grow();
+
+            // Divide if grown past threshold
+            if (b.L >= b.L_th) {
+            	CrossFeedingBacterium daughter = b.divide();
+            	
+            	bacBorn.add(daughter);  		// Add daughter to newborn class, 'mother' keeps her status
+            }
+        }
+        
+        bac.addAll(bacBorn); 					// Adds all the newborn daughters to sub-population
+        bacteriaAll.addAll(bacBorn); 			// Adds all the newborn daughters to total population
+        
+        // Allow daughter cells to grow 
+        for ( CrossFeedingBacterium b : bacBorn ) {
+        	
+            // Assigns a division length to each bacterium according to a normal distribution
+            double lengthThreshold = length_stdv*bacRng.nextGaussian()+length_mean;
+            b.setElongationThreshold(lengthThreshold);
+        }
+        
+        bacBorn.clear(); 						// Cleared for next time-step
+    }
+    
+    /** Function to remove bacteria due to cell death or by boundary. */
+    public void removeBacteria( BSim sim, ArrayList<CrossFeedingBacterium> bac, ArrayList<CrossFeedingBacterium> bac_dead ) {
+
+        for (CrossFeedingBacterium b : bac) {
+        	
+            // Kick out if past any boundary
+        	// Bacteria out of bounds = dead
+            if(b.position.x < 0 || b.position.x > sim.getBound().x || b.position.y < 0 || b.position.y > sim.getBound().y || b.position.z < 0 || b.position.z > sim.getBound().z){
+            	bac_dead.add(b);
+            } 
+            // Remove cell after it shrinks and becomes too small
+            if ( b.L <= 1 ) {
+            	bac_dead.add(b);
+            }
+        }
+        bac.removeAll(bac_dead);
+        
+        // Remove from total population
+        bacteriaAll.removeAll(bac_dead);
+        bac_dead.clear();
+    }
+    
+    // This one is a bit long too. Let's break it up
+    // 1. Begins an "action" -> this represents one timestep
+    // 2. Tells each bacterium to perform their action() function
+    // 3. Updates each chemical field in the simulation
+    // 4. Bacteria are then told to grow()
+    // 5. bacteria which are longer than their threshold are told to divide()
+    // 6. forces are applied and bacteria move around
+    // 7. bacteria which are out of bounds are removed from the simulation
+    @Override
+    public void tick() {
+    	
+        /********************************************** Action */
+    	
+        long startTimeAction = System.nanoTime(); 	// Wall-clock time, for diagnosing timing
+
+        // Calculates and stores the midpoint of the cell.
+        // Bacteria does action at each time step
+        for(BSimCapsuleBacterium b : bacteriaAll) {
+            b.action(); 							
+        }
+
+        // Outputs how long each step took, once every log interval.
+        long endTimeAction = System.nanoTime();
+        if((sim.getTimestep() % LOG_INTERVAL) == 0) {
+            System.out.println("Action update for " + bacteriaAll.size() + " bacteria took " + (endTimeAction - startTimeAction)/1e6 + " ms.");
+        }  
+
+        /********************************************** Chemical fields */
+        
+        startTimeAction = System.nanoTime();
+        
+        // Update the amino acid field
+        amino_acid_A.update(); 
+        amino_acid_B.update();
+
+        endTimeAction = System.nanoTime();
+        if((sim.getTimestep() % LOG_INTERVAL) == 0) {
+            System.out.println("Chemical field update took " + (endTimeAction - startTimeAction)/1e6 + " ms.");
+        }
+
+        /********************************************** Growth related activities if enabled. */
+        if (WITH_GROWTH) {
+
+            // ********************************************** Growth and division
+            startTimeAction = System.nanoTime(); 	// Start action timer
+
+            growBacteria( bacA, bac_bornA );		// For sub-population A
+            growBacteria( bacB, bac_bornB );		// For sub-population B
+
+            // Prints out information about bacteria when u want it to
+            endTimeAction = System.nanoTime();
+            if ((sim.getTimestep() % LOG_INTERVAL) == 0) {
+                System.out.println("Growth and division took " + (endTimeAction - startTimeAction) / 1e6 + " ms.");
+            }
+            
+            /********************************************** Neighbor interactions */
+            
+            startTimeAction = System.nanoTime();
+
+            mover.move();
+
+            endTimeAction = System.nanoTime();
+            if ((sim.getTimestep() % LOG_INTERVAL) == 0) {
+                System.out.println("Wall and neighbour interactions took " + (endTimeAction - startTimeAction) / 1e6 + " ms.");
+            }
+
+            /********************************************** Boundaries/removal */
+            startTimeAction = System.nanoTime();
+            
+            removeBacteria( sim, bacA, bac_deadA );		// For sub-population A
+            removeBacteria( sim, bacB, bac_deadB );		// For sub-population B
+            
+            endTimeAction = System.nanoTime();
+            if ((sim.getTimestep() % LOG_INTERVAL) == 0) {
+                System.out.println("Death and removal took " + (endTimeAction - startTimeAction) / 1e6 + " ms.");
+            }
+        }
+
+        startTimeAction = System.nanoTime();
+
+        endTimeAction = System.nanoTime();
+        if ((sim.getTimestep() % LOG_INTERVAL) == 0) {
+            System.out.println("Switch took " + (endTimeAction - startTimeAction) / 1e6 + " ms.");
+        }
+
+    }
+}
