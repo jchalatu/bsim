98d8c2786b5431d51363c92f0c3ecc023fbaa6af
s-yfang <55638266+s-yfang@users.noreply.github.com>
2021-04-12 13:20:13 -0400

    Update PhageFieldLogger.java
    Updated logger with more fields

--- run/BSimPhageLogger/PhageFieldLogger.java
+++ run/BSimPhageLogger/PhageFieldLogger.java
@@ -1,47 +1,77 @@
 package BSimPhageLogger;
 
-import java.io.IOException; 
-import java.util.ArrayList;
-
 import bsim.BSim;
+import bsim.capsule.BSimCapsuleBacterium;
 import bsim.export.BSimLogger;
 
-public class PhageFieldLogger extends BSimLogger {
-	
-	ArrayList<PhageFieldBacterium> bac;
+import java.io.IOException;
+import java.util.ArrayList;
 
-	public PhageFieldLogger(BSim sim, String filename, ArrayList<PhageFieldBacterium> bac) {
-		super(sim, filename);
-		this.bac = bac;
-	}
-    
+public class PhageFieldLogger extends BSimLogger {
+
+    ArrayList<PhageFieldBacterium> bac;
+    double pixel_to_um_ratio;
+
+    public PhageFieldLogger(BSim sim, String filename, ArrayList<PhageFieldBacterium> bac, double pixel_to_um_ratio) {
+        super(sim, filename);
+        this.bac = bac;
+        this.pixel_to_um_ratio = pixel_to_um_ratio;
+    }
+
     @Override
     public void before() {
         super.before();
-        String CellProfilerFields = "ImageNumber,ObjectNumber,"
-        		+ "AreaShape_MajorAxisLength,AreaShape_MinorAxisLength,AreaShape_Center_X,AreaShape_Center_Y,"
-        		+ "AreaShape_Orientation,AreaShape_Area\n";
-        write(CellProfilerFields);
+        String AreaShape_fields = "ImageNumber,ObjectNumber,AreaShape_Area,AreaShape_BoundingBoxArea," +
+                "AreaShape_BoundingBoxMaximum_X,AreaShape_BoundingBoxMaximum_Y,AreaShape_BoundingBoxMinimum_X," +
+                "AreaShape_BoundingBoxMinimum_Y,AreaShape_Center_X,AreaShape_Center_Y,AreaShape_Compactness," +
+                "AreaShape_Eccentricity,AreaShape_EquivalentDiameter,AreaShape_EulerNumber,AreaShape_Extent," +
+                "AreaShape_FormFactor,AreaShape_MajorAxisLength,AreaShape_MaxFeretDiameter,AreaShape_MaximumRadius," +
+                "AreaShape_MeanRadius,AreaShape_MedianRadius,AreaShape_MinFeretDiameter,AreaShape_MinorAxisLength," +
+                "AreaShape_Orientation,AreaShape_Perimeter,AreaShape_Solidity";
+        String TrackObjects_fields = "TrackObjects_Displacement_50,TrackObjects_DistanceTraveled_50,TrackObjects_FinalAge_50," +
+                "TrackObjects_IntegratedDistance_50,TrackObjects_Label_50,TrackObjects_Lifetime_50," +
+                "TrackObjects_Linearity_50,TrackObjects_ParentImageNumber_50,TrackObjects_ParentObjectNumber_50," +
+                "TrackObjects_TrajectoryX_50,TrackObjects_TrajectoryY_50";
+        String other_fields = "Location_Center_X,Location_Center_Y,Location_Center_Z,Number_Object_Number,Parent_filtered_objects_1_labeled";
+        String other_fields2 = "Location_Center_X,Location_Center_Y,Number_Object_Number,Parent_EditedObjects8,Parent_IdentifyPrimaryObjects7,Division_Angle";
+        //write(CellProfilerFields + "," + other2 + "\n");
+        write(AreaShape_fields + "," + other_fields + "," + TrackObjects_fields + "\n");
     }
 
-	@Override
-	public void during() {
+    @Override
+    public void during() {
+        String str = "-,";
+
         String buffer = "";
         for(PhageFieldBacterium b : bac) {
-        	double area = (Math.PI * b.radius * b.radius + 2 * b.radius * b.L) * 13.89 * 13.89;
-            buffer += ((int) (sim.getTimestep() * sim.getDt() / dt) + 1) + "," +
-                    (b.id + 1) + "," + 
-            		((b.L + 2 * b.radius) * BSimPhageField.pixel_to_um_ratio) + "," +
-            		(2 * b.radius) * BSimPhageField.pixel_to_um_ratio + "," +
-            		(b.position.x * BSimPhageField.pixel_to_um_ratio) + "," +
-            		(b.position.y * BSimPhageField.pixel_to_um_ratio) + "," +
-            		b.cell_profiler_angle() + "," +
-            		area +
-                    "\n";
+            double area = (Math.PI * b.radius * b.radius + 2 * b.radius * b.L) * 13.89 * 13.89;
+            int image_number = (int) (sim.getTimestep() * sim.getDt() / dt + 0.5) + 1;
+
+            String AreaShape_fields =  image_number + "," + (b.id + 1) + "," + area + "," + str.repeat(5) +
+                    (b.position.x * pixel_to_um_ratio) + "," +
+                    (b.position.y  * pixel_to_um_ratio) + "," + str.repeat(6) +
+                    (b.L + 2 * b.radius) * pixel_to_um_ratio + "," + str.repeat(5) +
+                    (2 * b.radius) * pixel_to_um_ratio + "," +
+                    b.cell_profiler_angle() + ",-,-";
+
+            /*
+            String other_fields = (b.position.x * pixel_to_um_ratio) + "," +
+                    (b.position.y * pixel_to_um_ratio) + "," +
+                    (b.id + 1) + "," + (b.id + 1) + "," + "-," +
+                    b.angle_initial;
+             */
+            String other_fields2 = (b.position.x * pixel_to_um_ratio) + "," +
+                    (b.position.y * pixel_to_um_ratio) + "," + "-," +
+                    (b.id + 1) + ",-";
+
+            String TrackObjects_fields = str.repeat(4) + (b.origin_id + 1) + "," + str.repeat(2) +
+                    (image_number - 1) + "," + (b.lifetime == 0 ? b.parent_id + 1 : b.id + 1) + ",-,-";
+
+            buffer += AreaShape_fields + "," + other_fields2 + "," + TrackObjects_fields + "\n";
         }
         write(buffer);
-	}
-	
+    }
+
     @Override
     public void write(String text) {
         try {
@@ -50,6 +80,6 @@ public class PhageFieldLogger extends BSimLogger {
             e.printStackTrace();
         }
     }
-	
 }
+
 
