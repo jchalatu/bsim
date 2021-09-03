fc3beaa42202e1458e4c23b1b419e4afce53786e
Jonathan Chalaturnyk <jchalatu@ualberta.ca>
2021-09-02 20:14:40 -0400

    Add correct BSIM_DIR environment variable to test script

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,12 +1,12 @@
 OUTDIR=`pwd -P`
-BSIMDIR=$(echo "$OUTDIR" | sed -e 's|/test||')
+BSIM_DIR=$(echo "$OUTDIR" | sed -e 's|/test||')
 EXPORTPATH="${OUTDIR}/test_output"
 FOLDER='benchmarking'
-RUNDIR="${BSIMDIR}/out/production/bsim-bristol/"
+RUNDIR="${BSIM_DIR}/out/production/bsim-bristol/"
 cd $RUNDIR
-JAVACMD="java -cp .:${BSIMDIR}/lib/* BasicSimulation2D.BasicSimulation2D"
+JAVACMD="java -cp .:${BSIM_DIR}/lib/* BasicSimulation2D.BasicSimulation2D"
 ARGS='-simt 1.0 -simdt 0.05 -export_time 0.05'
-DATAPATHS="-input_data ${BSIMDIR}/run/initialization_data/onecell-1800by1800.csv -export_path ${EXPORTPATH}/${FOLDER}"
+DATAPATHS="-input_data ${BSIM_DIR}/run/initialization_data/onecell-1800by1800.csv -export_path ${EXPORTPATH}/${FOLDER}"
 
 cmd="${JAVACMD} ${ARGS} ${DATAPATHS}"
 
